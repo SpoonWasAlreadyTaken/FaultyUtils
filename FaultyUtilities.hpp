2c1f1121b7aa66436ed46f263dd75edabd11cb2c
SpoonWasAlreadyTaken <laserpyoro@gmail.com>
2026-08-05 23:01:14 +0300

    more rands

--- FaultyUtilities.hpp
+++ FaultyUtilities.hpp
@@ -1,10 +1,27 @@
 #pragma once
 #include <random>
+#include <cmath>
 
 inline std::random_device rd;
 
-inline int RandomNumber(int min, int max) // generates a random number based on input parameters
-{
+inline int RandomInt(int min, int max) {
     std::uniform_int_distribution<> distr(min, max);
+    return distr(rd);
+}
+
+inline float RandomFloatPrecise(float min, float max, uint8_t precision) { /* slow but controls number of decimal points */
+    min *= (float)std::pow(10, precision);
+    max *= (float)std::pow(10, precision);
+    std::uniform_int_distribution<> distr(min, max);
+    return (float)distr(rd) / (float)precision;
+}
+
+inline float RandomFloat(float min, float max) {
+    std::uniform_real_distribution<float> distr(min, max);
+    return distr(rd);
+}
+
+inline double RandomDouble(double min, double max) {
+    std::uniform_real_distribution<double> distr(min, max);
     return distr(rd);
 }
